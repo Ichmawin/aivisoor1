93496cacd26a485c59f49340ffa19609ea0f91b3
Ichmawin <95000357+Ichmawin@users.noreply.github.com>
2026-04-24 15:50:50 +0100

    Update import path for settings in database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sess
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import DateTime, func
 from datetime import datetime
-from app.core.config import settings
+from config import settings
 import logging
 
 logger = logging.getLogger(__name__)
